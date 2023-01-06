0502d84f63d1d71999b459741720ee0ecb8cb7da
loumon koffi samuel <lkoffisamuel@gmail.com>
2023-01-06 13:35:42 +0000

    add dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,28 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+@extends('layouts.app')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+@section('content')
+    <main class="w-4/5 p-10 mx-auto mt-10 mb-20 bg-white shadow-2xl rounded-2xl">
+        <h1 class="py-5 text-4xl text-center">Tableau de board</h1>
+        <div class="py-12">
+            <div class="w-1/2">
+
+                <div class="flex text-xl justify-between my-2">
+                    <p>Nom: </p>
+                    <p> {{ auth()->user()->name }}</p>
+                </div>
+                <div class="flex text-xl justify-between my-2">
+                    <p>Email: </p>
+                    <p>{{ auth()->user()->email }}</p>
+                </div>
+                <div class="flex text-xl justify-between my-2">
+                    <p>Date d'inscription:</p>
+                    <p>{{ auth()->user()->created_at->format(' j M Y \\à h:i:s') }}</p>
                 </div>
             </div>
+            <div class="my-10">
+                <a href="{{ route('profile.edit') }}" class="bg-red-700 p-3 text-white rounded-lg">Modifier mon
+                    profile</a>
+            </div>
         </div>
-    </div>
-</x-app-layout>
+    </main>
+@endsection
